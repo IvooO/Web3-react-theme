7988512d4ec0fd07e6b0f46708c19fc0a9d57124
Ivooo <ivovanoverstraeten@hotmail.com>
2022-05-30 06:45:46 +0200

    update

--- src/pages/pricing.js
+++ src/pages/pricing.js
@@ -4,16 +4,21 @@ import PricingHeader from "../components/Pricing/pricingHeader"
 import PricingMain from "../components/Home/pricing"
 import PricingFAQ from "../components/Pricing/pricingFAQ"
 import PricingCTA from "../components/Pricing/pricingCTA"
+import AboutTeamBigCard from "../components/about/aboutTeamBigCard"
+import BlogsContainer from "../components/Blog/blogsContainer"
 
 const Pricing = () => {
   return (
     <Layout>
-      <PricingHeader></PricingHeader>
+      <AboutTeamBigCard></AboutTeamBigCard>
+      
+      <BlogsContainer></BlogsContainer>
+      {/* <PricingHeader></PricingHeader>
       <div className="mt-20">
         <PricingMain></PricingMain>
-      </div>
+      </div> */}
       <PricingFAQ></PricingFAQ>
-      <PricingCTA></PricingCTA>
+      {/* <PricingCTA></PricingCTA> */}
     </Layout>
   )
 }
